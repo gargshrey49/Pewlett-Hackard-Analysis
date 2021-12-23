5c1eb3d59c035e0cdfc0dfdd67c157378edc2a45
gargshrey49 <72582298+gargshrey49@users.noreply.github.com>
2021-12-23 11:06:20 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+CREATE TABLE  employees(
+	emp_no int NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	gender VARCHAR(40) NOT NULL,
+	hire_date date NOT NULL, 
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+CREATE TABLE titles (
+	emp_no int NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date date NOT NULL,
+	to_date date,
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_emp (
+	emp_no int NOT NULL,
+	dept_no VARCHAR(40) NOT NULL,
+	from_date date NOT NULL,
+	to_date date,
+	PRIMARY KEY (emp_no)
+);
